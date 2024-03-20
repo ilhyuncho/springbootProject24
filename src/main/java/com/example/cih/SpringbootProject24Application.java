24024a86f391643d9decc9f9830ed76e5f733b50
choilhyun <cihg155@gmail.com>
2024-03-20 10:37:00 +0900

    Initial commit

---
+++ src/main/java/com/example/cih/SpringbootProject24Application.java
@@ -0,0 +1,13 @@
+package com.example.cih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProject24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cih/SpringbootProject24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProject24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
