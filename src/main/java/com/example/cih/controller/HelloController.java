5f6d61a7472f3c3836b9de84afe5612554e04fcc
choilhyun <cihg155@gmail.com>
2024-03-20 11:47:21 +0900

    [log4j2 설정]- color 지정

--- src/main/java/com/example/cih/controller/HelloController.java
+++ src/main/java/com/example/cih/controller/HelloController.java
@@ -1,17 +1,21 @@
 package com.example.cih.controller;
 
 
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Log4j2
 public class HelloController {
 
     @GetMapping("/hello")
     public void hello(Model model){
 
-
+        log.info("hello()!!!");
+        log.error("error hello()!!!");
         // hello.html 으로 이동
         model.addAttribute("msg", "HELLO CIH!!");
     }
