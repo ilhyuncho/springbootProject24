9dda9aa6b7c0459ebace262b0fbe8fc1f669b955
choilhyun <cihg155@gmail.com>
2024-03-21 14:58:09 +0900

    [부트 스트랩 적용] sb-admin 적용, thymeleaf

---
+++ src/main/java/com/example/cih/controller/dashBoard/DashBoardController.java
@@ -0,0 +1,24 @@
+package com.example.cih.controller.dashBoard;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+@Log4j2
+public class DashBoardController {
+
+    @GetMapping("/list")
+    public void list(){
+        log.error("DashBoard list()~~~~~~~~~~");
+    }
+    @GetMapping("/charts")
+    public void charts(){
+        log.error("DashBoard charts()~~~~~~~~~~");
+    }
+
+}
