3c2a94aa5359854fa33f40b20440c94afe6f59a4
choilhyun <cihg155@gmail.com>
2024-03-21 10:56:42 +0900

    [Board Repository 위치 변경]

--- src/test/java/com/example/cih/domain/board/BoardTest.java
+++ src/test/java/com/example/cih/domain/board/BoardTest.java
@@ -1,14 +1,11 @@
 package com.example.cih.domain.board;
 
-import com.example.cih.repository.board.BoardRepository;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Log4j2
