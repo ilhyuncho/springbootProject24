2987083e6f78e654721e53f1bd689a9c7f46b894
choilhyun <cihg155@gmail.com>
2024-03-26 13:46:05 +0900

    [로그인, 회원가입 html 추가]

---
+++ src/main/java/com/example/cih/controller/auth/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.cih.controller.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Log4j2
+public class AuthController {
+
+    @GetMapping("/login")
+    public void login(){
+
+    }
+    @GetMapping("/register")
+    public void register(){
+
+    }
+
+}
