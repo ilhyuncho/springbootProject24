e09c4d3af9e5efce7304abdb9f9d60b80bfb8492
choilhyun <cihg155@gmail.com>
2024-03-29 12:52:09 +0900

    [파일 시스템] 여러개 파일 업로드 결과 전달.

--- src/main/java/com/example/cih/controller/fileUpload/UpDownController.java
+++ src/main/java/com/example/cih/controller/fileUpload/UpDownController.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -23,14 +25,17 @@ public class UpDownController {
     private String uploadPath;
     @ApiOperation(value="Upload Post", notes="POST 방식으로 파일 등록")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String upload(UploadFileDTO uploadFileDTO){
+    public List<UploadResultDTO> upload(UploadFileDTO uploadFileDTO){
         log.error("==================Upload Post=====================");
         log.error(uploadFileDTO);
+
+        List<UploadResultDTO> listResult = new ArrayList<>();
 
         if(uploadFileDTO.getFiles() != null){
             uploadFileDTO.getFiles().forEach(multipartFile -> {
                 String originalFileName = multipartFile.getOriginalFilename();
                 String uuid = UUID.randomUUID().toString();
+                boolean bImage = false;
 
                 Path savePath = Paths.get(uploadPath, uuid+"_"+originalFileName);
 
@@ -45,7 +50,13 @@ public class UpDownController {
                         File thumbFile = new File(uploadPath,  "s_" + uuid + "_" + originalFileName);
 
                         Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200,200);
+
+                        bImage = true;
                     }
+                    
+                    // 결과값 반환
+                    listResult.add(UploadResultDTO.builder().uuid(uuid).fileName(originalFileName)
+                            .img(bImage).build());
 
 
                 } catch (IOException e) {
@@ -54,6 +65,6 @@ public class UpDownController {
             });
         }
 
-        return null;
+        return listResult;
     }
 }
