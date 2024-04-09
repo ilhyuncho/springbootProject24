d3d30d38ed3a5b1d795aafa2d52af9cc3f425a9f
choilhyun <cihg155@gmail.com>
2024-04-09 13:00:10 +0900

    [JPA - cascade = CascadeType.ALL] - // 자식 엔티티도 같이 저장 되도록 ( PERSIST:엔티티가 저장 될때만

--- src/main/java/com/example/cih/domain/user/User.java
+++ src/main/java/com/example/cih/domain/user/User.java
@@ -29,6 +29,7 @@ public class User {
 
     @OneToMany(mappedBy = "user"        // 반대쪽 매핑의 필드 이름 값
             , fetch = FetchType.LAZY    // 지연 로딩 설정
+            , cascade = CascadeType.ALL     // 자식 엔티티도 같이 저장 되도록
     )
     @Builder.Default        // 빌더로 인스턴스 생성 시 초기화할 값을 정할 수 있음.
     private List<Car> ownCars = new ArrayList<>();  // 고객 소유 자동차 list
@@ -40,6 +41,13 @@ public class User {
 //    @OneToOne(mappedBy = "userCredit")  // 일대일-양방향에서 주인이 아니다
 //    private UserCredit userCredit;
 
+      public void addMyCars(Car car){
 
+          if( !ownCars.contains(car) ){
+              ownCars.add(car);
+          }
+          car.setUser(this);
+
+      }
 
 }
--- src/test/java/com/example/cih/domain/user/UserRepositoryTest.java
+++ src/test/java/com/example/cih/domain/user/UserRepositoryTest.java
@@ -1,17 +1,22 @@
 package com.example.cih.domain.user;
 
 import com.example.cih.domain.car.Car;
+import com.example.cih.domain.car.CarRepository;
+import com.example.cih.domain.car.CarSize;
 import lombok.extern.log4j.Log4j2;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.internal.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Log4j2
@@ -38,6 +43,36 @@ public class UserRepositoryTest {
         Long userId = userRepository.save(user).getUserId();
 
         Optional<User> user1 = userRepository.findByUserName("user1");
+        Assertions.assertEquals(user1.get().getUserName(), user.getUserName());
+    }
+
+    @Test
+    public void InsertUserWithCarsData(){
+
+        User user = User.builder()
+                .userName("user3")
+                .build();
+
+        List<Car> listCar = IntStream.rangeClosed(1, 2).mapToObj(i ->
+                Car.builder()
+                        .carModel("model10" + i)
+                        .carKm(10000L)
+                        .carColors("white")
+                        .carYears(2021)
+                        .carNumber("25마344" + i)
+                        .carGrade(CarSize.LARGE)
+                        .build()
+        ).collect(Collectors.toList());
+
+        //listCar.forEach(log::error);
+
+
+        // 유저 객체에 car 등록
+        listCar.forEach(user::addMyCars);
+
+        Long userId = userRepository.save(user).getUserId();
+
+        Optional<User> user1 = userRepository.findByUserName("user3");
         Assertions.assertEquals(user1.get().getUserName(), user.getUserName());
     }
 
