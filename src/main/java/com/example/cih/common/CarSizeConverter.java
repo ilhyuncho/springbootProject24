2e17c42a87fd16b8029d3161378be1e55a32b227
choilhyun <cihg155@gmail.com>
2024-04-21 14:36:07 +0900

    [JPA-이벤트 리스너 등록(3가지)] 엔티티 이벤트 리스너 지정

--- src/main/java/com/example/cih/common/CarSizeConverter.java
+++ src/main/java/com/example/cih/common/CarSizeConverter.java
@@ -6,6 +6,7 @@ import javax.persistence.AttributeConverter;
 import java.util.Objects;
 
 
+// 모든 CarSize 타입에 컨버터를 적용하려면 (autoplay = true) 옵션을 적용, @Convert를 삭제 해도 됨
 public class CarSizeConverter implements AttributeConverter<CarSize, String> {
     @Override
     public String convertToDatabaseColumn(CarSize attribute) {
--- src/main/java/com/example/cih/domain/car/Car.java
+++ src/main/java/com/example/cih/domain/car/Car.java
@@ -5,6 +5,7 @@ import com.example.cih.common.CarSizeConverter;
 import com.example.cih.domain.common.BaseEntity;
 import com.example.cih.domain.user.User;
 import lombok.*;
+import lombok.extern.log4j.Log4j2;
 
 import javax.persistence.*;
 
@@ -15,6 +16,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @ToString
 @Table(name="Cars")
+@Log4j2
 // 부모에게 상속받은 컬럼명 변경 시
 //@AttributeOverride(name="regDate", column = @Column(name="REG_DATE"))
 public class Car extends BaseEntity {
@@ -65,6 +67,40 @@ public class Car extends BaseEntity {
         this.carKm = carKm;
 
         this.user = user;   // 대입 방법 확인해 보자!!!
+    }
+
+
+    // 엔티티 리스너 등록 방법 (3가지)
+    // 1. 엔티티에 직접 적용
+    // 2. 별도의 리스너 등록 ( @EntityListeners )
+    // 3. 기본 리스너 사용 ( 모든 엔티티의 이벤트를 처리하려고
+
+    // 이벤트 호출 순서 : 1.기본 리스너 2.부모 클래스 리스너  3.리스너, 4.엔티티
+    
+    // @ExcludeDefaultListeners : 기본 리스너 무시
+    // @ExcludeSuperclassListeners : 상위 클래스 이벤트 리스너 무시
+
+    // 1.엔티티 이벤트 리스너 지정
+    @PrePersist
+    public void prePersist(){
+        // persist() 메소드를 호출해서 엔티티를 영속성 컨텍스트에 관리하기 직전에 호출
+        log.error("Car-prePersist()" + carId);
+    }
+
+    @PostPersist
+    public void postPersist(){
+        // flush나 commit 을 호출해서 엔티티를 DB에 저장한 직후
+        log.error("Car-PostPersist()" + carId);
+    }
+    @PostLoad
+    public void postLoad(){
+        // 엔티티가 영속성 컨텍스트에 조회된 직후 또는 refresh를 호출한 후 ( 2차 캐시에 저장되어 있어도 호출됨 )
+        log.error("Car-PostLoad()" + carId);
+    }
+    @PostRemove
+    public void postRemove(){
+        // flush나 commit을 호출해서 엔티티를 DB에 삭제한 직후에 호출
+        log.error("Car-PostRemove()" + carId);
     }
 
 
