24b6a32cc0fa4304cefb5b772d8550bcc09e1c44
choilhyun <cihg155@gmail.com>
2024-04-27 11:16:35 +0900

    [파일 업로드] CarImage 파일 업로드 화면 처리 - 등록, 삭제

---
+++ src/main/resources/static/js/upload.js
@@ -0,0 +1,20 @@
+async function uploadToServer(formObj) {
+
+    console.log("upload to Server....................")
+    console.log(formObj)
+
+    const response = await axios({
+        method: 'post',
+        url: '/upload',
+        data: formObj,
+        header:{'Content-Type' : 'multipart/form-data'}
+    });
+
+    return response.data
+}
+
+async function removeFileToServer(uuid, fileName){
+    const response = await axios.delete(`/remove/${uuid}_${fileName}`)
+
+    return response.data
+}
