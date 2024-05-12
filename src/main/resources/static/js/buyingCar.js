29691a700620e50e292891dbeca3d40ac4b1f5fb
choilhyun <cihg155@gmail.com>
2024-05-12 12:44:20 +0900

    [car 경매 기능 작업 20]  판매 차량이 해당 소유자 차이면 버튼 사라지게( purchaseOfferBtn.setAttribute("hidden", true) )

--- src/main/resources/static/js/buyingCar.js
+++ src/main/resources/static/js/buyingCar.js
@@ -23,10 +23,10 @@ async function cancelOffer(formObj) {
 
 async function getList({currentUser, sellingCarId, page, size}){
 
-    console.log({currentUser, sellingCarId, page, size})
+    //console.log({currentUser, sellingCarId, page, size})
 
     const result = await axios.get(`/buyingCar/list`, {params: {userName:currentUser, sellingCarId, page, size}})
 
-    console.log(result.data)
+    //console.log(result.data)
     return result.data
 }
