0de86f4e834f3be3b9ddb76fe1963f10d0d763c4
choilhyun <cihg155@gmail.com>
2024-05-23 11:29:22 +0900

    [내차 통계 페이지 1] 기초

--- src/main/java/com/example/cih/controller/myPage/HistoryController.java
+++ src/main/java/com/example/cih/controller/myPage/HistoryController.java
@@ -1,14 +1,12 @@
 package com.example.cih.controller.myPage;
 
 
-import com.example.cih.common.fileHandler.FileHandler;
 import com.example.cih.domain.user.User;
 import com.example.cih.service.car.UserCarService;
 import com.example.cih.service.user.UserService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,5 +33,18 @@ public class HistoryController {
 
         return "/myPage/carHistory";
     }
+
+    @ApiOperation(value = "내차 관리 통계 화면", notes = "")
+    @GetMapping("/statistics")
+    public String statistics(@ModelAttribute("carId") Long carId,
+                      String userName, Model model){
+
+        User user = userService.findUser(userName);
+
+        log.error("statistics-get : " + carId);
+
+        return "/myPage/carStatistics";
+    }
+
 
 }
