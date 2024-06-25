684eff03379fd00286a0841440a7fbcc0044ed8e
choilhyun <cihg155@gmail.com>
2024-06-25 15:32:21 +0900

    [악세서리 샵] 상세 페이지 작업1

--- src/main/java/com/example/cih/controller/shop/ShopController.java
+++ src/main/java/com/example/cih/controller/shop/ShopController.java
@@ -1,13 +1,14 @@
 package com.example.cih.controller.shop;
 
 import com.example.cih.dto.shop.ShopItemSimpleDTO;
+import com.example.cih.dto.shop.ShopItemViewDTO;
 import com.example.cih.service.shop.ShopItemService;
-import com.example.cih.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,4 +32,16 @@ public class ShopController {
 
         return "/shop/mainNew";
     }
+
+    @GetMapping("/itemDetail/{shopItemId}")
+    public String shopItemDetailOrModify(@PathVariable("shopItemId") Long shopItemId,
+                                         Model model){
+
+        ShopItemViewDTO shopItem = shopItemService.findItem(shopItemId);
+
+        model.addAttribute("responseDTO", shopItem);
+
+        return "/shop/itemDetail";
+    }
+
 }
