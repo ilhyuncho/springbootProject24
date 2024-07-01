63eef3dac5bfdf02573ad4b2aacb2f2a60449a1b
choilhyun <cihg155@gmail.com>
2024-07-01 10:38:38 +0900

    [코드 정리]

--- src/main/java/com/example/cih/controller/buyingCar/BuyingCarController.java
+++ src/main/java/com/example/cih/controller/buyingCar/BuyingCarController.java
@@ -1,18 +1,10 @@
 package com.example.cih.controller.buyingCar;
 
-import com.example.cih.dto.car.CarViewDTO;
-import com.example.cih.service.car.CarService;
-import com.example.cih.service.car.UserCarService;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/buyingCar")
@@ -21,27 +13,5 @@ import java.security.Principal;
 //@PreAuthorize("hasRole('USER')")
 public class BuyingCarController {
 
-    private final UserCarService userCarService;
-    private final CarService carService;
-
-    @ApiOperation(value = "[판매 차량 리스트]-번호 클릭", notes = "판매 차량 정보만 전달")
-    @GetMapping("/{carId}")
-    public String get(@PathVariable(name="carId") Long carId
-            ,Model model, Principal principal){
-
-        if(principal != null){
-            log.error(principal.getName());
-        }
-        else{
-            log.error("principal is null!!!!!!!!");
-        }
-
-        //CarViewDTO carViewDTO = userCarService.readMyCarDetailInfo( principal.getName(), carId);
-        CarViewDTO carViewDTO = carService.readOne(carId);
-
-        model.addAttribute("carViewDTO", carViewDTO);
-
-        return "/sellingCar/sellingCarInfo";
-    }
 
 }
--- src/main/java/com/example/cih/controller/sellingCar/SellingCarController.java
+++ src/main/java/com/example/cih/controller/sellingCar/SellingCarController.java
@@ -4,19 +4,21 @@ package com.example.cih.controller.sellingCar;
 
 import com.example.cih.dto.PageRequestDTO;
 import com.example.cih.dto.PageResponseDTO;
+import com.example.cih.dto.car.CarViewDTO;
 import com.example.cih.dto.sellingCar.SellingCarViewDTO;
+import com.example.cih.service.car.CarService;
 import com.example.cih.service.sellingCar.SellingCarService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/sellingCar")
@@ -26,6 +28,7 @@ import java.util.List;
 public class SellingCarController {
 
     private final SellingCarService sellingCarService;
+    private final CarService carService;
 
     @ApiOperation(value = "판매 차량 리스트 전달", notes = "[판매 차량 조회] 클릭시")
     @GetMapping("/list")
@@ -39,5 +42,48 @@ public class SellingCarController {
 
         return "/sellingCar/sellingCarList";
     }
+
+    @ApiOperation(value = "[판매 차량 정보 요청]", notes = "판매 차량 정보만 전달")
+    @GetMapping("/{carId}")
+    public String getCarInfo(@PathVariable(name="carId") Long carId
+            ,Model model, Principal principal){
+
+        if(principal != null){
+            log.error(principal.getName());
+        }
+        else{
+            log.error("principal is null!!!!!!!!");
+        }
+
+        //CarViewDTO carViewDTO = userCarService.readMyCarDetailInfo( principal.getName(), carId);
+        CarViewDTO carViewDTO = carService.readOne(carId);
+
+        model.addAttribute("carViewDTO", carViewDTO);
+
+        return "/sellingCar/sellingCarInfo";
+    }
+    @ApiOperation(value = "[판매 차량 정보 요청]", notes = "판매 차량 정보만 전달")
+    @GetMapping("/view/{carId}")
+    public String getCarView(@PathVariable(name="carId") Long carId
+            ,Model model, Principal principal){
+
+        if(principal != null){
+            log.error(principal.getName());
+        }
+        else{
+            log.error("principal is null!!!!!!!!");
+        }
+
+        //CarViewDTO carViewDTO = userCarService.readMyCarDetailInfo( principal.getName(), carId);
+        CarViewDTO carViewDTO = carService.readOne(carId);
+
+        model.addAttribute("carViewDTO", carViewDTO);
+
+        return "/sellingCar/sellingCarView";
+    }
+
+
+
+
 
 }
