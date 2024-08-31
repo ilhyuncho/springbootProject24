14910a74a9e425efd4b9a730a50bdd758d2492a2
choilhyun <cihg155@gmail.com>
2024-08-31 12:50:00 +0900

    [코드 정리]

---
+++ src/main/resources/static/js/utilEventHandler.js
@@ -0,0 +1,50 @@
+// 모달창의 [구매 제안], [경매 응찰 취소], [가격 수정] 처리
+document.querySelectorAll(".cancelBtn, .sendRegisterBtn, .sendModifyBtn").forEach(function(item,idx){
+    item.addEventListener("click", function (e) {
+        e.stopPropagation()
+        e.preventDefault()
+
+        const offerType = e.target.getAttribute("data-bs-target")
+
+        makeFormAndSendToServer(offerType)
+    })
+}, false)
+
+
+
+// [상담 요청], [경매 요청] 전송
+document.querySelectorAll(".sendConsultRequestBtn, .sendAuctionRequestBtn").forEach(function(item,idx){
+    item.addEventListener("click", function (e) {
+        e.stopPropagation()
+        e.preventDefault()
+
+        const offerType = e.target.getAttribute("data-bs-target")
+
+        makeFormAndSendToServer(offerType)
+    })
+}, false)
+
+// [모달창] 파일 업로드 닫기
+document.querySelectorAll(".closeUploadBtn").forEach(function (item,idx){
+    item.addEventListener('click', function(){
+
+        const fileInput = document.querySelector("input[name='files']")
+        console.log('closeUploadBtn click!!!!!!!!')
+
+        initModalFileUpload(fileInput)  // 파일 업로드 모달 창 입력 값 초기화
+        uploadModal.hide()
+    })
+}, false)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/resources/static/js/utilViewHtml.js
+++ src/main/resources/static/js/utilViewHtml.js
@@ -125,15 +125,4 @@ function viewCarLike(isLike){
     }
 }
 
-// [모달창] 파일 업로드 닫기
-document.querySelectorAll(".closeUploadBtn").forEach(function (item,idx){
-    item.addEventListener('click', function(){
-
-        const fileInput = document.querySelector("input[name='files']")
-        console.log('closeUploadBtn click!!!!!!!!')
-
-        initModalFileUpload(fileInput)  // 파일 업로드 모달 창 입력 값 초기화
-        uploadModal.hide()
-    })
-}, false)
 
