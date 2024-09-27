00d80f4835aebed9a39c33ef3cd26e49aa54f8e0
choilhyun <cihg155@gmail.com>
2024-09-27 10:51:24 +0900

    [테스트 환경 설정] @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)

--- src/test/java/com/example/cih/domain/reference/RefCarConsumableRepositoryTest.java
+++ src/test/java/com/example/cih/domain/reference/RefCarConsumableRepositoryTest.java
@@ -4,19 +4,36 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import java.util.Random;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Log4j2
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RefCarConsumableRepositoryTest {
 
     @Autowired
     private RefCarConsumableRepository refCarConsumableRepository;
 
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    int port;
+
     @Test
     public void testRandom(){
+
+        log.error("==================================");
+        log.error("port : " + port);
+        String result=testRestTemplate.getForObject("/",String.class);
+        assertThat(result).isEqualTo("hello namjune");
+
+
         Random random = new Random();
         // 4 개 생성, 2~ 9 까지
         random.ints(4, 2, 10).forEach(log::error);
