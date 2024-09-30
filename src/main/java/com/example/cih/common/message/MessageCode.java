d8de69029beffa9df5d8fc34bde99c9f8909c9e8
choilhyun <cihg155@gmail.com>
2024-09-30 12:12:59 +0900

    [html 통화 표시 변경] #numbers.formatCurrency -> #numbers.formatInteger

--- src/main/java/com/example/cih/common/message/MessageCode.java
+++ src/main/java/com/example/cih/common/message/MessageCode.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 public enum MessageCode {
 
     ALARM_REQUEST_CONSULT(1, "alarm.request.consult", "consultRequest"),
-    WELCOME_GREETING(2, "welcome.greeting", "none")
+    AUCTION_REQUEST_AUCTION(2, "alarm.request.auction", "auctionRequest"),
+    WELCOME_GREETING(3, "welcome.greeting", "none")
     ;
     private final int msgType;
     private final String msgCode;
